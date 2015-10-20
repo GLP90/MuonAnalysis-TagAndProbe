4e6e6dcead09fbb4a7f10cbcb41a46d5dc40c187
gael perrin <gael.ludovic.perrin@cern.ch>
2015-10-20 11:26:41 +0200

    .

--- test/zmumu/mu_POG/Production/crab_config.py
+++ test/zmumu/mu_POG/Production/crab_config.py
@@ -5,12 +5,12 @@ config = config()
 #!!
 #!!Change to run on other samples
 #!!
-config.General.requestName = 'DATA_50ns'
-config.General.workArea = 'crab_tnp_2015B_PRreference'
+#config.General.requestName = 'DATA_50ns'
+#config.General.workArea = 'crab_tnp_2015B_PRreference'
 #config.General.requestName = 'DATA_25ns'
 #config.General.workArea = 'crab_tnp_2015D_25ns'
-#config.General.requestName = 'NLO'
-#config.General.workArea = 'crab_tnp_MC_25ns'
+config.General.requestName = 'MCLO'
+config.General.workArea = 'crab_tnp_MC_25ns'
 #!!
 #!!DataSet
 #!!
@@ -18,7 +18,7 @@ config.General.workArea = 'crab_tnp_2015B_PRreference'
 #2015B
 #config.Data.inputDataset = '/SingleMuon/Run2015B-PromptReco-v1/AOD'
 #2015B 50ns (new calorimeter conditions)
-config.Data.inputDataset = '/SingleMuon/CMSSW_7_4_11_patch1-Run2015B_PRreference_74X_dataRun2_Prompt_v3-v1/RECO'
+#config.Data.inputDataset = '/SingleMuon/CMSSW_7_4_11_patch1-Run2015B_PRreference_74X_dataRun2_Prompt_v3-v1/RECO'
 #config.Data.inputDataset = '/SingleMuon/CMSSW_7_4_11_patch1-Run2015B_HLTreference_74X_dataRun2_HLT_v1-v1/RECO'
 #2015C
 #config.Data.inputDataset = '/SingleMuon/Run2015C-PromptReco-v1/AOD'
@@ -29,7 +29,7 @@ config.Data.inputDataset = '/SingleMuon/CMSSW_7_4_11_patch1-Run2015B_PRreference
 #config.Data.inputDataset = '/SingleMuon/Run2015D-PromptReco-v3/AOD'
 #MC
 #25ns LO
-#config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/AODSIM'
+config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-madgraphMLM-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v1/AODSIM'
 #25ns NLO
 #config.Data.inputDataset = '/DYJetsToLL_M-50_TuneCUETP8M1_13TeV-amcatnloFXFX-pythia8/RunIISpring15DR74-Asympt25ns_MCRUN2_74_V9-v3/AODSIM'
 #!!
@@ -46,8 +46,8 @@ config.Data.inputDataset = '/SingleMuon/CMSSW_7_4_11_patch1-Run2015B_PRreference
 #!!
 #!!config file
 #!!
-config.JobType.psetName = 'tp_from_aod_Data.py'
-#config.JobType.psetName = 'tp_from_aod_MC.py'
+#config.JobType.psetName = 'tp_from_aod_Data.py'
+config.JobType.psetName = 'tp_from_aod_MC.py'
 
 config.General.transferOutputs = True
 config.General.transferLogs = False
@@ -63,6 +63,6 @@ config.Data.splitting = 'LumiBased'
 config.Data.unitsPerJob = 50
 config.Data.outLFNDirBase = '/store/user/%s/' % getUsernameFromSiteDB()
 config.Data.publication = True
-config.Data.publishDataName = 'crab_tnp_production_data'
+config.Data.publishDataName = 'crab_tnp_production_mc_25ns_LO'
 config.Site.storageSite = 'T2_CH_CSCS'
 
