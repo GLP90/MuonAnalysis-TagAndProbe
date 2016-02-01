b3dc5ea52efc4229e687ce3d6259aca41200e9de
gael perrin <gael.ludovic.perrin@cern.ch>
2016-02-01 15:05:06 +0100

    added addNVtxWeight.cxx

--- test/zmumu/mu_POG/submit.sh
+++ test/zmumu/mu_POG/submit.sh
@@ -2,3 +2,7 @@
 
 #TEST
 bsub -q 8nh -u pippo1234 run.sh testsubmit 1 data_all data 
+
+#TEST SoftID
+bsub -q 8nh -u pippo1234 run.sh testsubmit 4 mc_all mclocal 
+#bsub -q 8nh -u pippo1234 run.sh testsubmit 4 data_all data 
